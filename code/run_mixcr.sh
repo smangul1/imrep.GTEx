d43fe4c4a6db1a44c1fea6684dd9c76a34dc3c73
Serghei Mangul <serghei@login1.(none)>
2017-10-07 10:51:36 -0700

    update script to run mixcr

--- code/run_mixcr.sh
+++ code/run_mixcr.sh
@@ -9,7 +9,7 @@ $mixcr assemblePartial -f -p alignments.vdjca alignments_rescued_1.vdjca
 $mixcr assemblePartial -f alignments_rescued_1.vdjca alignments_rescued_2.vdjca
 $mixcr assemble -f -r log_assemble.txt alignments_rescued_2.vdjca mixcr.clns
 $mixcr exportClones -f mixcr.clns mixcr.txt
-grep IGH mixcr.txt | grep -v "IGL" | grep -v "IGK" | cut -f 33 | grep -v "*" |  grep "^C" | grep "W$" | sort | uniq >mixcr_IGH_${sample}.cdr3
-grep IGK mixcr.txt | grep -v "IGL" | grep -v "IGH" | cut -f 33 | grep -v "*" |  grep "^C" | grep "F$" | sort | uniq >mixcr_IGK_${sample}.cdr3 
-grep IGL mixcr.txt | grep -v "IGK" | grep -v "IGH" | cut -f 33 | grep -v "*" |  grep "^C" | grep "F$" | sort | uniq >mixcr_IGL_${sample}.cdr3 
+#grep IGH mixcr.txt | grep -v "IGL" | grep -v "IGK" | cut -f 33 | grep -v "*" |  grep "^C" | grep "W$" | sort | uniq >mixcr_IGH_${sample}.cdr3
+#grep IGK mixcr.txt | grep -v "IGL" | grep -v "IGH" | cut -f 33 | grep -v "*" |  grep "^C" | grep "F$" | sort | uniq >mixcr_IGK_${sample}.cdr3 
+#grep IGL mixcr.txt | grep -v "IGK" | grep -v "IGH" | cut -f 33 | grep -v "*" |  grep "^C" | grep "F$" | sort | uniq >mixcr_IGL_${sample}.cdr3 
 
