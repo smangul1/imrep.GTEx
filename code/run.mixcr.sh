05541054c20b723b939ad456fbc4907f023e0bbf
Serghei Mangul <serghei@login4.(none)>
2018-03-30 11:59:09 -0700

    modify mixcr script based on recomendation of developers

--- code/run.mixcr.sh
+++ code/run.mixcr.sh
@@ -4,9 +4,10 @@ sample=$3
 mkdir $sample
 cd $sample
 
-$mixcr align -p rna-seq -r log_align.txt $1 $2 alignments.vdjca
+
+$mixcr align -p rna-seq -OallowPartialAlignments=true -r log_align.txt $1 $2 alignments.vdjca
 $mixcr assemblePartial -f -p alignments.vdjca alignments_rescued_1.vdjca
 $mixcr assemblePartial -f alignments_rescued_1.vdjca alignments_rescued_2.vdjca
-$mixcr assemble -f -r log_assemble.txt alignments_rescued_2.vdjca mixcr.clns
+$mixcr assemble -ObadQualityThreshold=0 -f -r log_assemble.txt alignments_rescued_2.vdjca mixcr.clns
 $mixcr exportClones -f mixcr.clns mixcr.txt
 
